a5a67d50eab8e390505ab0fc19b714a6524f3a29
James Halliday <mail@substack.net>
2012-03-12 00:04:36 -0700

    failing tests for special tags

---
+++ test/special.js
@@ -0,0 +1,22 @@
+var test = require('tap').test;
+var trumpet = require('../');
+var fs = require('fs');
+
+test('special tags', function (t) {
+    t.plan(1);
+    
+    var tr = trumpet();
+    fs.createReadStream(__dirname + '/special.html').pipe(tr);
+    
+    tr.select('.a .b', function (node) {
+        node.html(function (html) {
+            t.equal(html, 'boop');
+        });
+    });
+    
+    tr.select('.c .d', function (node) {
+        node.html(function (html) {
+            t.equal(html, 'wooo');
+        });
+    });
+});
