012c24e3262b9dfddac9bf60aa8a8b4a3d23a410
James Halliday <mail@substack.net>
2012-03-11 20:31:14 -0700

    failing tests for unimplemented new .update() and .remove() functions

---
+++ test/update.js
@@ -0,0 +1,34 @@
+var test = require('tap').test;
+var transit = require('../');
+var fs = require('fs');
+
+test('update', function (t) {
+    var html = fs.readFileSync(__dirname + '/update_target.html', 'utf8');
+    
+    var tr = transit();
+    fs.createReadStream(__dirname + '/update.html').pipe(tr);
+    
+    var spans = [ 'tacos', 'y', 'burritos' ];
+    
+    tr.select('.b span', function (node) {
+        node.update(function (html, emit) {
+            emit(html.toUpperCase());
+        });
+    });
+    
+    tr.select('.c', function (node) {
+        node.update('---');
+    });
+    
+    tr.select('.d', function (node) {
+        node.remove();
+    });
+    
+    var data = '';
+    tr.on('data', function (buf) { data += buf });
+    
+    tr.on('end', function () {
+        t.equal(html, data);
+        t.end();
+    });
+});
