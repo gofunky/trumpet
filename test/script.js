c5a89a7749b55775d7f7c1643683228fb6320809
Michael Matuzak <mmatuzak@gmail.com>
2012-06-01 16:43:23 -0700

    adding a test after the script tag that should fail

--- test/script.js
+++ test/script.js
@@ -3,7 +3,7 @@ var trumpet = require('../');
 var fs = require('fs');
 
 test('script', function (t) {
-    t.plan(2);
+    t.plan(6);
     
     var tr = trumpet();
     fs.createReadStream(__dirname + '/script.html').pipe(tr);
@@ -12,7 +12,20 @@ test('script', function (t) {
         t.equal(node.attributes.type, 'text/javascript');
         node.html(function (src) {
             t.equal(src, 'console.log(i<j)');
-            t.end();
+        });
+    });
+
+    tr.select('title', function (node) {
+        t.equal(node.name, 'title');
+        node.html(function (html) {
+            t.equal(html, 'beep');
+        });
+    });
+
+    tr.select('.c', function (node) {
+        t.equal(node.name, 'div');
+        node.html(function (html) {
+            t.equal(html, 'boop');
         });
     });
 });
