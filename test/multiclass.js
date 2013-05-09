97da8e25933cb10b2fd35e8d7f64446cf3f26caa
James Halliday <mail@substack.net>
2013-05-09 00:16:06 -0700

    more complicated multiclass passes by accident

--- test/multiclass.js
+++ test/multiclass.js
@@ -3,12 +3,19 @@ var trumpet = require('../');
 var fs = require('fs');
 
 test('multiclass', function (t) {
-    t.plan(2);
+    t.plan(4);
     
     var tr = trumpet();
     fs.createReadStream(__dirname + '/multiclass.html').pipe(tr);
     
     tr.select('.one', function (node) {
+        t.deepEqual(node.attributes, { 'class': 'one two' });
+        node.html(function (html) {
+            t.equal(html, 'xxx');
+        });
+    });
+    
+    tr.select('.one.two', function (node) {
         t.deepEqual(node.attributes, { 'class': 'one two' });
         node.html(function (html) {
             t.equal(html, 'xxx');
