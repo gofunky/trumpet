f3d088799fa94265f4201f6bafe4349987cd6eb8
James Halliday <mail@substack.net>
2013-06-13 16:03:53 -0700

    moved lexer-specific code into lib/lexer.js

--- example/through.js
+++ example/through.js
@@ -2,6 +2,8 @@ var trumpet = require('../');
 var through = require('through');
 
 var tr = trumpet();
+tr.select('div').setAttribute('xyz', '123');
+
 tr.pipe(process.stdout);
 
 var fs = require('fs');
