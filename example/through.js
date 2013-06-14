c17ce95f2aead7aceac128b143a74a1c2e41f32f
James Halliday <mail@substack.net>
2013-06-14 00:35:12 -0700

    equals, contains selectors

--- example/through.js
+++ example/through.js
@@ -2,7 +2,7 @@ var trumpet = require('../');
 
 var tr = trumpet();
 //tr.select('div#z').setAttribute('class', 'WOOOOOOOOOOOOOOOOOo');
-tr.select('input[type]').getAttribute('value', function (value) {
+tr.select('input[type~=text]').getAttribute('value', function (value) {
     console.log('value=' + value);
 });
 //tr.pipe(process.stdout);
