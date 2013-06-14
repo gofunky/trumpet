a8e246371c0fd3b7f7d5953b264f41ccef64b602
James Halliday <mail@substack.net>
2013-06-13 23:29:09 -0700

    matching a selector

--- example/through.js
+++ example/through.js
@@ -1,10 +1,11 @@
 var trumpet = require('../');
-var through = require('through');
 
 var tr = trumpet();
-tr.select('div.a').setAttribute('xyz', '123');
-
+tr.select('.a').setAttribute('xyz', '123');
 tr.pipe(process.stdout);
+
+// tr.createWriteStream('.a')
+// tr.select('.a').createWriteStream()
 
 var fs = require('fs');
 fs.createReadStream(__dirname + '/through.html').pipe(tr);
--- index.js
+++ index.js
@@ -1,35 +1,49 @@
-var sax = require('sax');
 var through = require('through');
-var buffers = require('buffers');
 var duplexer = require('duplexer');
 var tokenize = require('./lib/tokenize.js');
 var parseSelector = require('./lib/selector.js');
-
-var EVENTS = [
-    'opentag', 'attribute', 'opencdata', 'closecdata',
-    'closetag', 'script', 'comment', 'text'
-];
+var matcher = require('./lib/matcher.js');
 
 module.exports = function (opts) {
     var tokens = tokenize();
     var selectors = [];
     
     var dup = duplexer(tokens, tokens.pipe(through(function (lex) {
-        this.queue(lex[1]);
-        //console.dir([ lex[0], lex[1] + '' ]);
+        selectors.forEach(function (s) {
+            s._at(lex[0], lex[2]);
+        });
+        //this.queue(lex[0]);
+        console.dir([ lex[0], lex[1] + '' ]);
     })));
     
-    dup.select = function (s) {
-        var sel = parseSelector(s);
-        console.dir(sel[0].attribute);
-        selectors.push(sel);
-        return new Result;
+    dup.select = function (sel) {
+        var r = new Result(sel);
+        selectors.push(r);
+        return r;
     };
     return dup;
 };
 
-function Result () {
+function Result (sel) {
+    this._setAttr = {};
+    this._matcher = matcher(parseSelector(sel));
 }
 
+Result.prototype._at = function (kind, x) {
+    this._matcher.at(kind, x);
+};
+
 Result.prototype.setAttribute = function (key, value) {
+    this._setAttr[key] = value;
+    return this;
+};
+
+Result.prototype.createWriteStream = function () {
+    // pipe data into this selector!
+    // doesn't work with selectAll()
+};
+
+Result.prototype.createReadStream = function () {
+    // pipe data FROM this selector!
+    // doesn't work with selectAll()
 };
