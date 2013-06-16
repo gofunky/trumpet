1cde0fe6281fb5a0b5da8afa9a06b1a673fbb5e3
James Halliday <mail@substack.net>
2013-06-16 01:21:25 -0700

    non-matching descendant test also passes

--- test/descendant.js
+++ test/descendant.js
@@ -1,6 +1,7 @@
 var trumpet = require('../');
 var fs = require('fs');
 var test = require('tape');
+var through = require('through');
 
 test('descendant selector', function (t) {
     t.plan(1);
@@ -11,4 +12,17 @@ test('descendant selector', function (t) {
         t.equal(value, 'abc');
     });
     fs.createReadStream(__dirname + '/descendant.html').pipe(tr);
+});
+
+test('descendant no-match selector', function (t) {
+    t.plan(1);
+    
+    var tr = trumpet();
+    var elem = tr.select('.a .d');
+    elem.getAttribute('class', function (value) {
+        t.fail('should not have matched');
+    });
+    fs.createReadStream(__dirname + '/descendant.html').pipe(tr);
+    
+    tr.pipe(through(null, function () { t.ok(true) }));
 });
