787a5bcc7b8ea6bca193926a5f85850ce5ae105b
James Halliday <mail@substack.net>
2013-06-15 18:09:25 -0700

    failing set attribute test

---
+++ test/set_attr.js
@@ -0,0 +1,17 @@
+var trumpet = require('../');
+var fs = require('fs');
+var test = require('tape');
+var concat = require('concat-stream');
+
+test('set attribute', function (t) {
+    t.plan(1);
+    
+    var tr = trumpet();
+    var elem = tr.select('input[type=text]');
+    elem.setAttribute('value', 'abc');
+    
+    tr.pipe(concat(function (src) {
+        t.equal(src, '<div class="a"><input type="text" value="abc"></div>\n');
+    }));
+    fs.createReadStream(__dirname + '/set_attr.html').pipe(tr);
+});
