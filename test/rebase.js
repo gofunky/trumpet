d394a84c770737e0a72607d3c15b119fe4d45d58
James Halliday <mail@substack.net>
2013-06-16 02:20:43 -0700

    rebase selector test just works somehow

--- test/rebase.js
+++ test/rebase.js
@@ -3,13 +3,24 @@ var fs = require('fs');
 var test = require('tape');
 var through = require('through');
 
-test('rebase selector', function (t) {
+test('wonky duplicated classes selector', function (t) {
     t.plan(1);
     
     var tr = trumpet();
     var elem = tr.select('.c');
     elem.getAttribute('class', function (value) {
         t.equal(value, 'c');
+    });
+    fs.createReadStream(__dirname + '/rebase.html').pipe(tr);
+});
+
+test('rebase selector', function (t) {
+    t.plan(1);
+    
+    var tr = trumpet();
+    var elem = tr.select('.a > .b > * > .d');
+    elem.getAttribute('class', function (value) {
+        t.equal(value, 'd');
     });
     fs.createReadStream(__dirname + '/rebase.html').pipe(tr);
 });
