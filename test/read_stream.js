444242535f7154c43f536145691c95ec57a6477d
James Halliday <mail@substack.net>
2013-06-18 20:53:34 -0700

    simultaneous read stream and get attribute calls work

--- test/read_stream.js
+++ test/read_stream.js
@@ -4,12 +4,18 @@ var test = require('tape');
 var concat = require('concat-stream');
 
 test('read stream', function (t) {
-    t.plan(1);
+    t.plan(3);
     
     var tr = trumpet();
     
     tr.select('.a').createReadStream().pipe(concat(function (body) {
         t.equal(body.toString(), 'AAA');
+    }));
+    
+    var b = tr.select('.b');
+    b.getAttribute('class', function (v) { t.equal(v, 'b') });
+    b.createReadStream().pipe(concat(function (body) {
+        t.equal(body.toString(), 'X<b>Y</b>Z');
     }));
     
     fs.createReadStream(__dirname + '/read_stream.html').pipe(tr);
