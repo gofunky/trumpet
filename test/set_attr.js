0ab8b959072e1d673367ad21ca1dbda43f311f19
James Halliday <mail@substack.net>
2013-06-19 05:42:36 -0700

    failing create attribute test

--- test/set_attr.js
+++ test/set_attr.js
@@ -18,3 +18,19 @@ test('set attribute', function (t) {
     }));
     fs.createReadStream(__dirname + '/set_attr.html').pipe(tr);
 });
+
+test('create attribute', function (t) {
+    t.plan(1);
+    
+    var tr = trumpet();
+    var elem = tr.select('input[type=text]');
+    elem.setAttribute('beep', 'boop');
+    
+    tr.pipe(concat(function (src) {
+        t.equal(
+            String(src),
+            '<div class="a"><input type="text" value="xyz" beep="boop"></div>\n'
+        );
+    }));
+    fs.createReadStream(__dirname + '/set_attr.html').pipe(tr);
+});
