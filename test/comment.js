5adc2f3c89ca5021d3b706782dad954405e80d3b
James Halliday <mail@substack.net>
2013-07-30 15:37:16 -0700

    an extra comment test just to make sure

--- test/comment.js
+++ test/comment.js
@@ -3,7 +3,7 @@ var through = require('through');
 var test = require('tape');
 var concat = require('concat-stream');
 
-test('write stream', function (t) {
+test('comment write stream', function (t) {
     t.plan(1);
 
     var tr = trumpet();
@@ -15,6 +15,27 @@ test('write stream', function (t) {
     s.end();
 
     var res = '<!-- test --><html></html>';
+    tr.pipe(concat(function (body) {
+        t.equal(
+            body.toString(),
+            res
+        );
+    }));
+    tr.end(res);
+});
+
+test('comment write stream variant', function (t) {
+    t.plan(1);
+
+    var tr = trumpet();
+    var html = tr.select('html');
+    var ws = html.createWriteStream();
+
+    var s = through();
+    s.pipe(ws);
+    s.end();
+
+    var res = '<!--test   --><html></html>';
     tr.pipe(concat(function (body) {
         t.equal(
             body.toString(),
