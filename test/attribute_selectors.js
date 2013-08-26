96a8de73b2088edff162724cc024a93869a8581a
Ruud <r.bijnen@gmail.com>
2013-08-26 16:41:36 +0200

    added substring matching attribute selectors (CSS3)

--- test/attribute_selectors.js
+++ test/attribute_selectors.js
@@ -5,7 +5,7 @@ var concat = require('concat-stream');
 
 test('selected element has attribute [att]', function (t) {
     t.plan(1);
-    
+
     var tr = trumpet();
     tr.createReadStream('li[data-foo]').pipe(concat(function (elem) {
         t.equal(elem.toString(), 'item2');
@@ -15,7 +15,7 @@ test('selected element has attribute [att]', function (t) {
 
 test('selected element has attribute value [att=val]', function (t) {
     t.plan(1);
-    
+
     var tr = trumpet();
     tr.createReadStream('li[class="item1 item-first"]').pipe(concat(function (elem) {
         t.equal(elem.toString(), 'item1');
@@ -23,9 +23,9 @@ test('selected element has attribute value [att=val]', function (t) {
     fs.createReadStream(__dirname + '/attribute_selectors.html').pipe(tr);
 });
 
-test('selected element contains whitespace seperated attribute value [att~=val]', function (t) {
+test('selected element contains whitespace separated attribute value [att~=val]', function (t) {
     t.plan(1);
-    
+
     var tr = trumpet();
     tr.createReadStream('li[class~=item-first]').pipe(concat(function (elem) {
         t.equal(elem.toString(), 'item1');
@@ -37,12 +37,57 @@ test('selected element attribute value equals or starts with and followed by - [
 
     var items = ['item1', 'item3', 'item4'];
     t.plan(items.length);
-    
+
     var tr = trumpet();
     tr.selectAll('li[data-role|=item]', function (elem) {
-		elem.createReadStream().pipe(concat(function (src) {
-	        t.equal(src.toString(), items.shift());
-	    }));
+        elem.createReadStream().pipe(concat(function (src) {
+            t.equal(src.toString(), items.shift());
+        }));
+    });
+
+    fs.createReadStream(__dirname + '/attribute_selectors.html').pipe(tr);
+});
+
+test('selected element has attribute (att) value starting with (val) [att^=val]', function (t) {
+
+    var items = ['item1', 'item2', 'item3', 'item4'];
+    t.plan(items.length);
+
+    var tr = trumpet();
+    tr.selectAll('li[class^=item]', function (elem) {
+        elem.createReadStream().pipe(concat(function (src) {
+            t.equal(src.toString(), items.shift());
+        }));
+    });
+
+    fs.createReadStream(__dirname + '/attribute_selectors.html').pipe(tr);
+});
+
+test('selected element has attribute (att) value ending with (val) [att$=val]', function (t) {
+
+    var items = ['item4'];
+    t.plan(items.length);
+
+    var tr = trumpet();
+    tr.selectAll('li[data-role$=last]', function (elem) {
+        elem.createReadStream().pipe(concat(function (src) {
+            t.equal(src.toString(), items.shift());
+        }));
+    });
+
+    fs.createReadStream(__dirname + '/attribute_selectors.html').pipe(tr);
+});
+
+test('selected element has attribute (att) value containing (val) [att*=val]', function (t) {
+
+    var items = ['item2', 'item5'];
+    t.plan(items.length);
+
+    var tr = trumpet();
+    tr.selectAll('li[data-foo*=random]', function (elem) {
+        elem.createReadStream().pipe(concat(function (src) {
+            t.equal(src.toString(), items.shift());
+        }));
     });
 
     fs.createReadStream(__dirname + '/attribute_selectors.html').pipe(tr);
