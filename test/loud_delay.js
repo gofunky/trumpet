d17f628400b7f42c1f466e560f82e4d9404d2ddb
James Halliday <mail@substack.net>
2014-05-13 14:01:01 +0200

    failing test for delayed stream results

---
+++ test/loud_delay.js
@@ -0,0 +1,26 @@
+var test = require('tape');
+var trumpet = require('../');
+var through = require('through');
+var concat = require('concat-stream');
+var fs = require('fs');
+var expected = fs.readFileSync(__dirname + '/loud_expected.html', 'utf8');
+
+test('loud', function (t) {
+    t.plan(1);
+    var tr = trumpet();
+
+    var loud = tr.select('.loud').createStream();
+    loud.pipe(through(function (buf) {
+        var self = this;
+        setTimeout(function () {
+            self.queue(buf.toString().toUpperCase());
+        }, 10);
+    })).pipe(loud);
+    
+    fs.createReadStream(__dirname + '/loud.html')
+        .pipe(tr)
+        .pipe(concat(function (src) {
+            t.equal(src.toString('utf8'), expected);
+        }))
+    ;
+});
