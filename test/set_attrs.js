fc8dcbefb31487193d84d5b92184864403787704
James Halliday <mail@substack.net>
2014-06-02 13:50:52 -0700

    fix for set attributes test

--- test/set_attrs.js
+++ test/set_attrs.js
@@ -8,7 +8,7 @@ test('set attributes', function (t) {
     
     var tr = trumpet();
     tr.selectAll('input[type=text]', function (elem) {
-        elem.setAttribute('value', elem.attributes.value.toUpperCase());
+        elem.setAttribute('value', elem.getAttribute('value').toUpperCase());
     });
     
     tr.pipe(concat(function (src) {
