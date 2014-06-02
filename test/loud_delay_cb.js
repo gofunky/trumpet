2cc0770f5dc51be1d2c49812b1de4f6b35452573
James Halliday <mail@substack.net>
2014-06-01 23:32:26 -0700

    failing delayed cb test

---
+++ test/loud_delay_cb.js
@@ -0,0 +1,28 @@
+var test = require('tape');
+var trumpet = require('../');
+var through = require('through');
+var concat = require('concat-stream');
+var fs = require('fs');
+var expected = fs.readFileSync(__dirname + '/loud_expected.html', 'utf8');
+
+test('loud delay cb', function (t) {
+    t.plan(1);
+    var tr = trumpet();
+    
+    tr.select('.loud', function (elem) {
+        var loud = elem.createStream();
+        loud.pipe(through(function (buf) {
+            var self = this;
+            setTimeout(function () {
+                self.queue(buf.toString().toUpperCase());
+            }, 10);
+        })).pipe(loud);
+    });
+    
+    fs.createReadStream(__dirname + '/loud.html')
+        .pipe(tr)
+        .pipe(concat(function (src) {
+            t.equal(src.toString('utf8'), expected);
+        }))
+    ;
+});
