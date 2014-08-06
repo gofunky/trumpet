621f798aab7e6af41917c3391b7cfcee5235a83a
James Halliday <mail@substack.net>
2014-08-06 23:20:01 +1200

    immediately writing still works hmm

--- test/write_end.js
+++ test/write_end.js
@@ -24,3 +24,25 @@ test('write end', function (t) {
     
     fs.createReadStream(__dirname + '/write_end.html').pipe(tr);
 });
+
+test('write end string', function (t) {
+    t.plan(1);
+    
+    var tr = trumpet();
+    tr.select('.x b', function (elem) {
+        var ws = elem.createWriteStream();
+        ws.end('beep boop');
+    });
+    
+    tr.pipe(concat(function (body) {
+        t.equal(
+            body.toString(),
+            '<!doctype html>\n'
+            + '<html>\n<body>\n<div class="x"><b>beep boop</b></div>\n'
+            + '</body>\n</html>\n'
+        );
+    }));
+    
+    var html = fs.readFileSync(__dirname + '/write_end.html', 'utf8');
+    tr.end(html);
+}); 
